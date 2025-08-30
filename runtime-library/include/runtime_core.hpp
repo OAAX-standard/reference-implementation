66c721fad82a3334bfdca5f1a98f884f52c06bc4
Ayoub Assis <assis.ayoub@gmail.com>
2025-08-30 22:02:42 +0200

    fix compilation bug
    Signed-off-by: Ayoub Assis <assis.ayoub@gmail.com>

--- runtime-library/include/runtime_core.hpp
+++ runtime-library/include/runtime_core.hpp
@@ -10,22 +10,22 @@
 #define EXPOSE_FUNCTION __attribute__((visibility("default")))
 #endif
 
-EXPOSE_FUNCTION extern "C" int runtime_initialization_with_args(int length, char **keys, void **values);
+extern "C" EXPOSE_FUNCTION int runtime_initialization_with_args(int length, char **keys, void **values);
 
-EXPOSE_FUNCTION extern "C" int runtime_initialization();
+extern "C" EXPOSE_FUNCTION int runtime_initialization();
 
-EXPOSE_FUNCTION extern "C" int runtime_model_loading(const char *model_path);
+extern "C" EXPOSE_FUNCTION int runtime_model_loading(const char *model_path);
 
-EXPOSE_FUNCTION extern "C" int send_input(tensors_struct *input_tensors);
+extern "C" EXPOSE_FUNCTION int send_input(tensors_struct *input_tensors);
 
-EXPOSE_FUNCTION extern "C" int receive_output(tensors_struct **output_tensors);
+extern "C" EXPOSE_FUNCTION int receive_output(tensors_struct **output_tensors);
 
-EXPOSE_FUNCTION extern "C" int runtime_destruction();
+extern "C" EXPOSE_FUNCTION int runtime_destruction();
 
-EXPOSE_FUNCTION const char *runtime_error_message();
+extern "C" EXPOSE_FUNCTION const char *runtime_error_message();
 
-EXPOSE_FUNCTION const char *runtime_version();
+extern "C" EXPOSE_FUNCTION const char *runtime_version();
 
-EXPOSE_FUNCTION const char *runtime_name();
+extern "C" EXPOSE_FUNCTION const char *runtime_name();
 
 #endif // RUNTIME_CORE_HPP
